0d4cd59a76690e27ddbe50f1c0ca75a1865bd808
Patrick Choi <ptrckchoi@gmail.com>
2023-02-22 11:37:10 -0800

    added console log for canvas

--- src/game.js
+++ src/game.js
@@ -124,7 +124,12 @@ class Game {
     //what happens when you click on screen and not on specific html elem
   let mouseY = e.clientY - 36; //36 is font size of header1
   let mouseX = e.clientX
+  const canvasPosition = this.canvas.getBoundingClientRect();
+  let offsetX = canvasPosition.left;
+  let offsetY = canvasPosition.top;
 
+  console.log(offsetX, offsetY)
+  
   this.resetDialogue();
   if (Utils.isMouseOnRect([mouseX, mouseY], this.bagon)){
       this.bagon.clickedOn();
